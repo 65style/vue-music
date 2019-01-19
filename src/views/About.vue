749081e6ba64f8d4e8bae49ac8a2995088294943
lym210119 <lym210119@gmail.com>
2019-01-19 19:24:52 +0800

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <svg-icon icon-class="plus" class-name="icon-plus"/>
   </div>
 </template>
+
+<style>
+.icon-plus {
+  font-size: 100px;
+  color: red;
+}
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="vm-login">
+    <div class="login-content">
+      <div class="logo">
+        <svg-icon
+          icon-class="logo2"
+          class-name="logo-class"
+        ></svg-icon>
+      </div>
+      <router-link
+        class="btn-login"
+        to="/login/phone"
+      >手机号登陆</router-link>
+      <router-link
+        class="btn-login"
+        to="/login/email"
+      >邮箱登录</router-link>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.vm-login {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: url(~@/assets/login-bg.jpg) no-repeat;
+  background-size: cover;
+
+  .login-content {
+    width: 80%;
+    height: auto;
+
+    .logo {
+      padding: p2r(0.5rem);
+      .logo-class {
+        display: block;
+        margin: 0 auto;
+        width: 230px;
+        height: 44px;
+      }
+    }
+
+    .btn-login {
+      display: block;
+      line-height: p2r(0.8rem);
+      border: 1px solid $primaryColor;
+      color: #fff;
+      font-size: $f_auto_l;
+      border-radius: p2r(0.5rem);
+      margin: p2r(0.3rem) 0;
+    }
+  }
+}
+</style>
